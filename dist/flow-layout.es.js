e5878c4f8db95e87c3d20ea0719910aca5c3f360
youpinyao <youpinyao@anta.com>
2025-08-25 13:27:24 +0800

    feat: init

--- dist/flow-layout.es.js
+++ dist/flow-layout.es.js
@@ -1,72 +1,72 @@
 var x = Object.defineProperty;
-var v = (a, t, o) => t in a ? x(a, t, { enumerable: !0, configurable: !0, writable: !0, value: o }) : a[t] = o;
-var h = (a, t, o) => v(a, typeof t != "symbol" ? t + "" : t, o);
+var v = (a, e, o) => e in a ? x(a, e, { enumerable: !0, configurable: !0, writable: !0, value: o }) : a[e] = o;
+var h = (a, e, o) => v(a, typeof e != "symbol" ? e + "" : e, o);
 class T {
-  constructor(t) {
+  constructor(e) {
     h(this, "options");
     h(this, "debounceTimeout", null);
     this.options = {
       gap: 10,
-      ...t
+      ...e
     }, this.initialize();
   }
   getContainer() {
-    const { container: t } = this.options;
-    return typeof t == "string" ? document.querySelector(t) : t;
+    const { container: e } = this.options;
+    return typeof e == "string" ? document.querySelector(e) : e;
   }
   getItems() {
-    const { items: t } = this.options;
-    return [t].flat().map((o) => typeof o == "string" ? Array.from(
+    const { items: e } = this.options;
+    return [e].flat().map((o) => typeof o == "string" ? Array.from(
       document.querySelectorAll(o)
     ) : o).flat();
   }
   initialize() {
-    const t = this.getContainer();
-    t.style.position = "relative";
+    const e = this.getContainer();
+    e.style.position = "relative";
     const o = new ResizeObserver(() => {
       this.debounceUpdate();
     });
-    new MutationObserver((i) => {
-      this.debounceUpdate(), i.forEach((s) => {
-        s.addedNodes.forEach((r) => {
+    new MutationObserver((s) => {
+      this.debounceUpdate(), s.forEach((i) => {
+        i.addedNodes.forEach((r) => {
           r instanceof HTMLElement && (o.unobserve(r), o.observe(r));
-        }), s.removedNodes.forEach((r) => {
+        }), i.removedNodes.forEach((r) => {
           r instanceof HTMLElement && o.unobserve(r);
         });
       });
-    }).observe(t, {
+    }).observe(e, {
       childList: !0,
       subtree: !0
-    }), o.observe(t);
+    }), o.observe(e);
   }
   update() {
-    const { gap: t = 10, appendToMinHeightColumn: o = !1 } = this.options, u = this.getContainer(), i = {
+    const { gap: e = 10, appendToMinHeightColumn: o = !1 } = this.options, u = this.getContainer(), s = {
       x: 0
-    }, s = {}, r = this.getItems(), d = (e) => (e == null ? void 0 : e.reduce((n, c) => n + c.offsetHeight + t, 0)) || 0, b = (e) => parseInt(getComputedStyle(e).paddingLeft.replace("px", "")) + parseInt(getComputedStyle(e).paddingRight.replace("px", ""));
-    r.forEach((e) => {
+    }, i = {}, r = this.getItems(), d = (t) => (t == null ? void 0 : t.reduce((n, c) => n + c.offsetHeight + e, 0)) || 0, g = (t) => parseInt(getComputedStyle(t).paddingLeft.replace("px", "")) + parseInt(getComputedStyle(t).paddingRight.replace("px", ""));
+    r.forEach((t) => {
       var c;
-      const n = e.nextElementSibling;
-      if (e instanceof HTMLElement && (e.style.position = "absolute", e.style.transform = `translate(${i.x}px, ${d(
-        s[i.x]
-      )}px)`, s[i.x] = [...s[i.x] || [], e], i.x += e.offsetWidth + t, i.x + ((n == null ? void 0 : n.offsetWidth) ?? 0) > u.clientWidth - b(e) && (i.x = 0), o && // 判断当前列是否有元素
-      d(s[i.x]) !== 0)) {
-        const p = Object.entries(s).map(([l, f]) => [
+      const n = t.nextElementSibling;
+      if (t instanceof HTMLElement && (t.style.position = "absolute", t.style.transform = `translate(${s.x}px, ${d(
+        i[s.x]
+      )}px)`, i[s.x] = [...i[s.x] || [], t], s.x += t.offsetWidth + e, s.x + ((n == null ? void 0 : n.offsetWidth) ?? 0) > u.clientWidth - g(u) && (s.x = 0), o && // 判断当前列是否有元素
+      d(i[s.x]) !== 0)) {
+        const p = Object.entries(i).map(([l, f]) => [
           parseFloat(l),
-          f.reduce((g, m) => g + m.offsetHeight + t, 0)
+          f.reduce((b, m) => b + m.offsetHeight + e, 0)
         ]).sort((l, f) => l[1] - f[1]);
-        i.x = ((c = p == null ? void 0 : p[0]) == null ? void 0 : c[0]) ?? 0;
+        s.x = ((c = p == null ? void 0 : p[0]) == null ? void 0 : c[0]) ?? 0;
       }
-    }), u.style.height = `${Object.values(s).map(
-      (e) => e.reduce((n, c) => n + c.offsetHeight + t, 0)
-    ).reduce((e, n) => Math.max(e, n), 0) - t}px`;
+    }), u.style.height = `${Object.values(i).map(
+      (t) => t.reduce((n, c) => n + c.offsetHeight + e, 0)
+    ).reduce((t, n) => Math.max(t, n), 0) - e}px`;
   }
   debounceUpdate() {
     this.debounceTimeout && clearTimeout(this.debounceTimeout), this.debounceTimeout = setTimeout(() => {
-      this.update();
-    }, 100);
+      console.log(333), this.update();
+    }, 50);
   }
-  updateOptions(t) {
-    this.options = { ...this.options, ...t }, this.debounceUpdate();
+  updateOptions(e) {
+    this.options = { ...this.options, ...e }, this.debounceUpdate();
   }
 }
 export {
